9e43fdbeb65996b21ed63af2221ed6a5cb6342b1
Dave Horsfall <dave@horsfall.dev>
2023-02-15 15:19:57 +0000

    init schema work

--- src/api/organ/controllers/organ.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * organ controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::organ.organ');
--- src/api/organ/routes/organ.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * organ router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::organ.organ');
