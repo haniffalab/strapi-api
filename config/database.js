5616ec361b042e703305b5d4b6ab11af7169b7b8
dannda <dann.3004@gmail.com>
2024-01-30 10:54:10 +0000

    add dev and prod database config
    update packages

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
+const path = require("path");
+
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: "sqlite",
     connection: {
-      socketPath: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USER"),
-      password: env("DATABASE_PASSWORD"),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
+    useNullAsDefault: true,
   },
 });
