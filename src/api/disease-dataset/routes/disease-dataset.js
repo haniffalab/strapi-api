52f8a9a2ca91348223bcaa46bae4ee71354b00c7
dannda <dann.3004@gmail.com>
2024-04-24 12:53:10 +0100

    add disease-dataset and disease-gene content types

---
+++ src/api/disease-dataset/routes/disease-dataset.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * disease-dataset router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::disease-dataset.disease-dataset');
---
+++ src/api/disease-gene/routes/disease-gene.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * disease-gene router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::disease-gene.disease-gene');
