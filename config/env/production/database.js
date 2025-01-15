d5e96096e40402c44dc68e1f7210ad91f710d3aa
Dave Horsfall <dave@horsfall.dev>
2025-01-15 14:35:29 +0000

    upgrade node 20

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: 'mysql',
     connection: {
-      socketPath: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USER"),
-      password: env("DATABASE_PASSWORD"),
+      socketPath: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USER'),
+      password: env('DATABASE_PASSWORD'),
     },
   },
 });
