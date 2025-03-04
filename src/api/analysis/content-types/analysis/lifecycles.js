ab08564a943bbbae32a5c57f71ddcf82752b2218
dannda <dann.3004@gmail.com>
2025-03-04 11:44:34 +0000

    check if update is publish/unpublish

--- src/api/analysis/content-types/analysis/lifecycles.js
+++ src/api/analysis/content-types/analysis/lifecycles.js
@@ -10,15 +10,19 @@ module.exports = {
   async beforeUpdate(event) {
     const { data, where } = event.params;
 
-    const entry = await strapi.entityService.findOne('api::analysis.analysis', where.id);
+    const isPublishAction = 'publishedAt' in data;
+
+    if (!isPublishAction){
+      const entry = await strapi.entityService.findOne('api::analysis.analysis', where.id);
     
-    if ('name' in data && data.name !== entry.name){
-      event.params.data.uid = await strapi.service('plugin::content-manager.uid')
-        .generateUIDField({
-          contentTypeUID: 'api::analysis.analysis',
-          field: 'uid',
-          data: data
-        });
+      if ('name' in data && data.name !== entry.name){
+        event.params.data.uid = await strapi.service('plugin::content-manager.uid')
+          .generateUIDField({
+            contentTypeUID: 'api::analysis.analysis',
+            field: 'uid',
+            data: data
+          });
+      }
     }
   },
 };
--- src/api/publication/content-types/publication/lifecycles.js
+++ src/api/publication/content-types/publication/lifecycles.js
@@ -10,15 +10,19 @@ module.exports = {
   async beforeUpdate(event) {
     const { data, where } = event.params;
 
-    const entry = await strapi.entityService.findOne('api::publication.publication', where.id);
+    const isPublishAction = 'publishedAt' in data;
+
+    if (!isPublishAction){
+      const entry = await strapi.entityService.findOne('api::publication.publication', where.id);
     
-    if ('title' in data && data.title !== entry.title){
-      event.params.data.uid = await strapi.service('plugin::content-manager.uid')
-        .generateUIDField({
-          contentTypeUID: 'api::publication.publication',
-          field: 'uid',
-          data: data
-        });
+      if ('title' in data && data.title !== entry.title){
+        event.params.data.uid = await strapi.service('plugin::content-manager.uid')
+          .generateUIDField({
+            contentTypeUID: 'api::publication.publication',
+            field: 'uid',
+            data: data
+          });
+      }
     }
   },
 
