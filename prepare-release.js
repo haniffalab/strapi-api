dc18c476ad43b9e91e2fe0c6fe08eb2a058537bb
Dave Horsfall <dave@horsfall.dev>
2025-08-18 10:45:40 +0100

    fix: pass updated version to prep script

--- prepare-release.js
+++ prepare-release.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const newVersion = process.argv[2];
+
 // Path to your CITATION.cff
 const cffPath = path.join(__dirname, "CITATION.cff");
 
@@ -8,14 +10,9 @@ const cffPath = path.join(__dirname, "CITATION.cff");
 const file = fs.readFileSync(cffPath, "utf8");
 
 // Replace the version line (like `version: "1.1.3"`)
-const newFile = file.replace(
-  /^version: .*/m,
-  `version: "${process.env.NEXT_RELEASE_VERSION}"`
-);
+const newFile = file.replace(/^version: .*/m, `version: "${newVersion}"`);
 
 // Write the updated file
 fs.writeFileSync(cffPath, newFile);
 
-console.log(
-  `Updated CITATION.cff to version ${process.env.NEXT_RELEASE_VERSION}`
-);
+console.log(`Updated CITATION.cff to version ${newVersion}`);
