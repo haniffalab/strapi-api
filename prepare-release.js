462c7b2bc0d09602da610b418c479abea232df2b
Dave Horsfall <dave@horsfall.dev>
2025-08-18 10:57:52 +0100

    feat: support multiple files for version bump

--- prepare-release.js
+++ prepare-release.js
@@ -3,16 +3,16 @@ const path = require("path");
 
 const newVersion = process.argv[2];
 
-// Path to your CITATION.cff
-const cffPath = path.join(__dirname, "CITATION.cff");
+// List of files to update
+const filesToUpdate = ["CITATION.cff"];
 
-// Read the file
-const file = fs.readFileSync(cffPath, "utf8");
+filesToUpdate.forEach((relativePath) => {
+  const filePath = path.join(__dirname, relativePath);
+  const file = fs.readFileSync(filePath, "utf8");
 
-// Replace the version line (like `version: "1.1.3"`)
-const newFile = file.replace(/^version: .*/m, `version: "${newVersion}"`);
+  // Replace the version line (like `version: "X.Y.Z"`)
+  const newFile = file.replace(/^version: .*/m, `version: "${newVersion}"`);
 
-// Write the updated file
-fs.writeFileSync(cffPath, newFile);
-
-console.log(`Updated CITATION.cff to version ${newVersion}`);
+  fs.writeFileSync(filePath, newFile);
+  console.log(`Updated ${relativePath} to version ${newVersion}`);
+});
